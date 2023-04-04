d321e30ed8b3016be010d2d7fa2a143ef33609db
Monsangter <97347454+Monsangter@users.noreply.github.com>
2023-04-04 10:53:29 +0900

    [level 2] Title: 숫자의 표현, Time: 5.31 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -10,30 +10,21 @@ def solution(n):
     #마지막항은 무엇이 되는가.
     # 항이 5개. 가능 왜 4개는 안됨?
     
-#     for i in range(1,n+1):
-#         for j in range(i,n+1):
-#             sum1 += j
-#             if sum1 == n:
-#                 answer += 1
-#                 break
+    for i in range(1,n+1):
+        for j in range(i,n+1):
+            sum1 += j
+            if sum1 == n:
+                answer += 1
+                break
+            elif sum1 > n:
+                break
             
-#         sum1 = 0
+            
+        sum1 = 0
+        
+    return answer
                 
 
                 
         
     
-#     return answer
-
-def solution(n):
-    count = 0                      
-    for i in range(1, n+1):         # 예시의 `15=15`도 있기 때문에 n+1 까지 반복문 실행
-        sumN = 0                     
-        for j in range(i, n+1):     # i값을 시작으로 반복문 실행
-            sumN += j               # i값부터 계속해서 값을 더해준다
-            if sumN == n:           # 더한 값이(sumN)이 n과 같다면 count +1, break
-                count += 1          
-                break
-            if sumN > n:            # 더한 값(sumN)이 n보다 크다면 계산할 필요가 없음
-                break
-    return count
