a61fc0cfb5fb742488bf29f9a0b35fab60a5f150
Monsangter <97347454+Monsangter@users.noreply.github.com>
2023-04-07 05:15:35 +0900

    [Bronze II] Title: 블랙잭, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,25 @@
+from sys import stdin
+from itertools import combinations
+
+def main():
+  def input():
+    return stdin.readline().rstrip()
+    
+  N,M = map(int,input().split())
+
+  ls1= list(map(int,input().split()))
+
+  ls2 = combinations(ls1 , 3)
+
+  tmp = 0
+  pro = 300000
+
+  for i in ls2:
+    if abs(M - sum(i))< pro and sum(i) <= M:
+      tmp = sum(i)
+      pro = abs(M-sum(i))
+
+  print(tmp)
+  
+if __name__ == "__main__":
+  main()
